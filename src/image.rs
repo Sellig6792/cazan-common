c00c4a40ee6f95fa76770f6d4e30d5d39f35b463
Gilles Meunier <sellig6792@gmail.com>
2024-05-09 18:54:41 +0200

    Make it work with holes

--- src/image.rs
+++ src/image.rs
@@ -79,81 +79,113 @@ impl ImagePolygon {
         panic!("No opaque points found");
     }
 
-    pub fn to_polygon(&self) -> Polygon {
-        let mut points = Vec::new();
+    pub fn to_polygon(&self) -> (Polygon, Vec<Polygon>) {
+        let mut holes: Vec<Polygon> = Vec::new();
         let edge_points = self.get_edges_points();
 
-        let mut current_point = self.get_first_point_of_main_polygon();
+        let current_point = self.get_first_point_of_main_polygon();
 
-        loop {
-            points.push(current_point);
-            let right_point = edge_points
-                .iter()
-                .find(|&point| Some(point) == current_point.right().as_ref());
-            let right_up_point = edge_points
-                .iter()
-                .find(|&point| Some(point) == current_point.right_up().as_ref());
-            let right_down_point = edge_points
-                .iter()
-                .find(|&point| Some(point) == current_point.right_down().as_ref());
-            let up_point = edge_points
-                .iter()
-                .find(|&point| Some(point) == current_point.up().as_ref());
-            let down_point = edge_points
-                .iter()
-                .find(|&point| Some(point) == current_point.down().as_ref());
-            let left_point = edge_points
-                .iter()
-                .find(|&point| Some(point) == current_point.left().as_ref());
-            let left_up_point = edge_points
-                .iter()
-                .find(|&point| Some(point) == current_point.left_up().as_ref());
-            let left_down_point = edge_points
-                .iter()
-                .find(|&point| Some(point) == current_point.left_down().as_ref());
+        let polygon = get_polygon_from_point(current_point, edge_points.clone());
 
-            // Check if the point is not the last point of the polygon and if it is not already in the list
-            match (
-                right_point,
-                right_up_point,
-                right_down_point,
-                up_point,
-                down_point,
-                left_point,
-                left_up_point,
-                left_down_point,
-            ) {
-                (Some(point), _, _, _, _, _, _, _) if !points.contains(point) => {
-                    current_point = *point;
+        // If there's some points that are in the edge_points list but not in the polygon list, it means that there's holes
+        let mut remaining_points = edge_points
+            .iter()
+            .filter(|point| !polygon.contains(point))
+            .cloned()
+            .collect::<Vec<_>>();
+
+        if edge_points.len() > polygon.len() {
+            loop {
+                if remaining_points.is_empty() {
+                    break;
                 }
-                (_, Some(point), _, _, _, _, _, _) if !points.contains(point) => {
-                    current_point = *point;
-                }
-                (_, _, Some(point), _, _, _, _, _) if !points.contains(point) => {
-                    current_point = *point;
-                }
-                (_, _, _, Some(point), _, _, _, _) if !points.contains(point) => {
-                    current_point = *point;
-                }
-                (_, _, _, _, Some(point), _, _, _) if !points.contains(point) => {
-                    current_point = *point;
-                }
-                (_, _, _, _, _, Some(point), _, _) if !points.contains(point) => {
-                    current_point = *point;
-                }
-                (_, _, _, _, _, _, Some(point), _) if !points.contains(point) => {
-                    current_point = *point;
-                }
-                (_, _, _, _, _, _, _, Some(point)) if !points.contains(point) => {
-                    current_point = *point;
-                }
-                _ => break,
+
+                let hole = get_polygon_from_point(remaining_points[0], remaining_points.clone());
+
+                remaining_points = remaining_points
+                    .iter()
+                    .filter(|point| !hole.contains(point))
+                    .cloned()
+                    .collect::<Vec<_>>();
+
+                holes.push(hole);
             }
         }
-        points
+
+        (polygon, holes)
     }
 }
 
+fn get_polygon_from_point(point: Point, all_points: Vec<Point>) -> Polygon {
+    let mut polygon: Polygon = Vec::new();
+    let mut current_point = point;
+    loop {
+        polygon.push(current_point);
+        let right_point = all_points
+            .iter()
+            .find(|&point| Some(point) == current_point.right().as_ref());
+        let right_up_point = all_points
+            .iter()
+            .find(|&point| Some(point) == current_point.right_up().as_ref());
+        let right_down_point = all_points
+            .iter()
+            .find(|&point| Some(point) == current_point.right_down().as_ref());
+        let up_point = all_points
+            .iter()
+            .find(|&point| Some(point) == current_point.up().as_ref());
+        let down_point = all_points
+            .iter()
+            .find(|&point| Some(point) == current_point.down().as_ref());
+        let left_point = all_points
+            .iter()
+            .find(|&point| Some(point) == current_point.left().as_ref());
+        let left_up_point = all_points
+            .iter()
+            .find(|&point| Some(point) == current_point.left_up().as_ref());
+        let left_down_point = all_points
+            .iter()
+            .find(|&point| Some(point) == current_point.left_down().as_ref());
+
+        match (
+            right_point,
+            right_up_point,
+            right_down_point,
+            up_point,
+            down_point,
+            left_point,
+            left_up_point,
+            left_down_point,
+        ) {
+            (Some(point), _, _, _, _, _, _, _) if !polygon.contains(point) => {
+                current_point = *point;
+            }
+            (_, Some(point), _, _, _, _, _, _) if !polygon.contains(point) => {
+                current_point = *point;
+            }
+            (_, _, Some(point), _, _, _, _, _) if !polygon.contains(point) => {
+                current_point = *point;
+            }
+            (_, _, _, Some(point), _, _, _, _) if !polygon.contains(point) => {
+                current_point = *point;
+            }
+            (_, _, _, _, Some(point), _, _, _) if !polygon.contains(point) => {
+                current_point = *point;
+            }
+            (_, _, _, _, _, Some(point), _, _) if !polygon.contains(point) => {
+                current_point = *point;
+            }
+            (_, _, _, _, _, _, Some(point), _) if !polygon.contains(point) => {
+                current_point = *point;
+            }
+            (_, _, _, _, _, _, _, Some(point)) if !polygon.contains(point) => {
+                current_point = *point;
+            }
+            _ => break,
+        }
+    }
+
+    polygon
+}
 impl From<Vec<u8>> for ImagePolygon {
     fn from(data: Vec<u8>) -> Self {
         Self::new(image::load_from_memory(&data).unwrap())
